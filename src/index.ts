e6cce98758010f78f9c6d7ce7ca2666f170d30a0
Sabeer Sulaiman <sabeer@fantacode.com>
2017-11-11 14:35:34 +0530

    Changed filter row color + fixes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { DataTable } from './components';
 export { DataTableModule } from './datatable.module';
+export { DataTableResource } from './tools/data-table-resource';
+export { DataTableTranslations, DataTableParams } from './components/types';
